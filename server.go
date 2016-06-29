60d0418d2e5d262215164ccd1c4c0af92a43baf7
Walter Sagehorn <w.sagehorn@gmail.com>
2016-06-29 15:10:49 -0500

    Added basic functionality
    can add/update robots with /update and view with /json and /text

--- server.go
+++ server.go
@@ -1,21 +1,36 @@
 package main
 
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
+
 //imports
 
-type robot struct {
-	name      string // the name of the robot
-	ip        string // the ip address of the robot
-	position  []int  // array of postition coordinates
-	alive     bool   // if the robot is currently active
-	lastAlive int    // epoch time since robot last pinged server
-
+//Robot must be exported so that json package can access it to encode
+type Robot struct {
+	Name      string // the name of the robot
+	IP        string // the ip address of the robot
+	X         string // x coordinate
+	Y         string // y coordinate
+	Alive     string // if the robot is currently active
+	LastAlive string // epoch time since robot last pinged server
 }
 
 //declare variables
-var robots []robot
+var robots []Robot
 
 var portNumber string
-var debug bools
+var file string
+var debug bool
+
+var saveTicker = time.NewTicker(20 * time.Second) // controls time between saves
 
 //define functions for server endpoints
 ////    /update           called by robots with their info
@@ -24,10 +39,118 @@ var debug bools
 ////    /hostsjson        legacy support, serves json of robotname:IP
 ////    /hostsalivejson   legacy support, serves json of robotname:IP of active robots
 
-//utility functions
-//// save
-//// load
-//// jsonify
-//// prettify
+func update(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	pdebug("IP of request: " + r.RemoteAddr)
+	fmt.Fprintf(w, updateRobot(query, r.RemoteAddr))
+}
 
-//
+func getJSON(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, jsonOutput())
+}
+
+func getText(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, textOutput())
+}
+
+//utility functions
+
+func pdebug(message string) {
+	if debug {
+		fmt.Println(message)
+	}
+}
+
+//error helper, prints error message if there's an error
+func checkErr(err error, message string) {
+	if err != nil {
+		log.Printf("Error: %s\n", message)
+		log.Println(err)
+	}
+}
+
+func save() {
+	pdebug("saving to local file")
+	bytes, err := json.Marshal(robots)
+	checkErr(err, "couldn't marshal the DNS")
+	err = ioutil.WriteFile(file, bytes, 0644)
+	checkErr(err, "couldn't write to "+file)
+}
+
+func periodicallySave() {
+	for {
+		save()
+		<-saveTicker.C // waits on ticker
+	}
+}
+
+func load() {
+	pdebug("reading from " + file)
+	bytes, err := ioutil.ReadFile(file)
+	checkErr(err, "couldn't read from "+file)
+	err = json.Unmarshal(bytes, &robots)
+	checkErr(err, "couldn't unmarshal data read from "+file)
+}
+
+func addRobot(query url.Values, addr string) {
+	bot := Robot{
+		Name: query.Get("name"),
+		IP:   addr,
+		X:    query.Get("x"),
+		Y:    query.Get("y")}
+	pdebug("Adding new robot: " + string(bot.Name))
+	robots = append(robots, bot)
+}
+
+func updateRobot(query url.Values, addr string) string {
+	for i, bot := range robots {
+		if bot.Name == query.Get("name") {
+			robots[i].IP = addr
+			robots[i].X = query.Get("x")
+			robots[i].Y = query.Get("y")
+			pdebug("Updated " + query.Get("name"))
+			return "updated " + query.Get("name")
+		}
+	}
+	addRobot(query, addr) // if it's not in robots, add it
+	return "Added " + query.Get("name")
+}
+
+func jsonOutput() string {
+	bytes, err := json.Marshal(robots)
+	checkErr(err, "couldn't jsonify")
+	return string(bytes)
+}
+
+//// prettify
+func textOutput() string {
+	ret := ""
+	for _, bot := range robots {
+		ret += bot.Name + "\n"
+		ret += "\t" + "IP: " + bot.IP + "\n"
+		ret += "\t" + "Coordinates: (" + bot.X + ", " + bot.Y + ")\n"
+		ret += "\t" + "Time Last Alive: " + bot.LastAlive + "\n"
+	}
+	return ret
+}
+
+func main() {
+	flag.BoolVar(&debug, "debug", false, "print debug info")
+	flag.StringVar(&portNumber, "port", "7978", "port number")
+	flag.StringVar(&file, "file", ".robot_statuses", "file to save robot statuses")
+	flag.Parse()
+
+	//load from local files/database
+	load()
+
+	//start goroutine to periodically save?
+	go periodicallySave()
+
+	//bind/start server
+	http.HandleFunc("/update", update)
+	http.HandleFunc("/json", getJSON)
+	http.HandleFunc("/text", getText)
+
+	fmt.Println("starting server on port " + portNumber)
+	log.Fatal(http.ListenAndServe(":"+portNumber, nil))
+}
